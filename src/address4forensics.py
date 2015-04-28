762a626837c7303e585491ee5ed3224e92bc2e1f
Jon Yocky <curlyfries13@comcast.net>
2015-04-27 18:47:14 -0700

    Complete implementation of forensic calculator

--- src/address4forensics.py
+++ src/address4forensics.py
@@ -1,38 +1,312 @@
 import sys
 import re
-from enum import enum
 
 # first arg will always be this script
 args = sys.argv[1:]
 argsLen = len(sys.argv)
-address4forensics(args)
 
 DEFAULT_PARTITION_OFFSET = 0
 DEFAULT_SECTOR_SIZE = 512
 DEFAULT_FAT_TABLES = 2
 
-def address4forensics(self, args):
-    modes = solveModes(args)
+DEFAULT = 1000
+LOGICAL = 1001
+PHYSICAL = 1002
+CLUSTER = 1003
 
+def main():
+    address4forensics(args)
+
+def address4forensics(args):
+    modes = solveModes(args)
+    calculate(modes)
+
+
+def calculate(mode):
+    if(mode[0] == LOGICAL):
+        calculateLogical(mode[1:])
+    elif(mode[0] == PHYSICAL):
+        calculatePhysical(mode[1:])
+    elif(mode[0] == CLUSTER):
+        calculateCluster(mode[1:])
+
+def calculateLogical(mode):
+    # get all the variables declared and initialized here
+    result = -1
+
+    offset = -1
+    sectorSize = -1
+    physical = -1
+    cluster = -1
+
+    clusterSize = -1
+    reservedSectors = -1
+    fatTables = -1
+    fatLength = -1
+
+
+    offset = mode[0]
+    if(mode[1]):
+        sectorSize = mode[2]
+    else:
+        sectorSize = DEFAULT_SECTOR_SIZE
+    if(mode[3] != -1):
+        # this is the only argument we need in this case
+        print mode[3]
+        exit();
+    if(mode[4] != -1):
+        physical = mode[4]
+    if(mode[5] != -1):
+        cluster = mode[5]
+        clusterSize = mode[6]
+        reservedSectors = mode[7]
+        fatTables = mode[8]
+        fatLength = mode[9]
+
+    if(physical == -1 and cluster == -1):
+        print 'Error: no address to convert'
+        exit();
+    elif(physical != -1 and cluster != -1):
+        print 'Error: conflicting address command, only enter one known address type'
+        exit();
+    elif(cluster != -1):
+
+        if(clusterSize < 0 or reservedSectors < 0 or fatTables < 0 or fatLength < 0):
+            comma = False;
+            errorMessage = 'Error: '
+            if(clusterSize < 0):
+                if(comma):
+                    errorMessage+=', '
+                else:
+                    comma = True;
+                errorMessage += 'Invalid cluster size'
+            elif(reservedSectors < 0):
+                if(comma):
+                    errorMessage+=', '
+                else:
+                    comma = True;
+                errorMessage += 'Invalid number of reserved sectors'
+            elif(fatTables < 0):
+                if(comma):
+                    errorMessage +=', '
+                else:
+                    comma = True;
+                errorMessage += 'Invalid number of FAT tables'
+            elif(fatLength < 0):
+                if(comma):
+                    errorMessage +=', '
+                else:
+                    comma = True;
+                errorMessage += 'Invalid FAT length'
+            print errorMessage
+            exit()
+        else:
+            # solve the logical address based on cluster information
+            result = reservedSectors + (fatTables * fatLength) + (cluster - 2) * clusterSize
+            print result
+            exit();
+
+    elif(physical != -1):
+        # solve logical address based on physical information
+        result = physical - offset
+        print result
+        exit();
+    print "Unknown Error"
+    exit();
+
+def calculatePhysical(mode):
+    result = -1
+
+    offset = -1
+    sectorSize = -1
+    logical = -1
+    cluster = -1
+
+    clusterSize = -1
+    reservedSectors = -1
+    fatTables = -1
+    fatLength = -1
+
+    offset = mode[0]
+    if(mode[1]):
+        sectorSize = mode[2]
+    else:
+        sectorSize = DEFAULT_SECTOR_SIZE
+    if(mode[3] != -1):
+        logical = mode[3]
+    if(mode[4] != -1):
+        # this is the only argument we need in this case
+        print mode[4]
+        exit();
+    if(mode[5] != -1):
+        cluster = mode[5]
+        clusterSize = mode[6]
+        reservedSectors = mode[7]
+        fatTables = mode[8]
+        fatLength = mode[9]
+
+    # check for errors
+    if(logical == -1 and cluster == -1):
+        print 'Error: no address to convert'
+        exit();
+    elif(logical != -1 and cluster != -1):
+        print 'Error conflicting address command, only enter one known address'
+        exit();
+    elif(cluster != -1):
+
+        if(clusterSize < 0 or reservedSectors < 0 or fatTables < 0 or fatLength < 0):
+            comma = False;
+            errorMessage = 'Error: '
+            if(clusterSize < 0):
+                if(comma):
+                    errorMessage+=', '
+                else:
+                    comma = True;
+                errorMessage += 'Invalid cluster size'
+            elif(reservedSectors < 0):
+                if(comma):
+                    errorMessage+=', '
+                else:
+                    comma = True;
+                errorMessage += 'Invalid number of reserved sectors'
+            elif(fatTables < 0):
+                if(comma):
+                    errorMessage+=', '
+                else:
+                    comma = True;
+                errorMessage += 'Invalid number of FAT tables'
+            elif(fatLength < 0):
+                if(comma):
+                    errorMessage+=', '
+                else:
+                    comma = True;
+                errorMessage += 'Invalid FAT length'
+            print errorMessage
+            exit()
+        else:
+            # solve the logical address based on cluster information
+            result = offset + reservedSectors + (fatTables * fatLength) + ((cluster - 2) * clusterSize)
+            print result
+            exit()
+    elif(logical != -1):
+        result = logical + offset
+        print result
+        exit();
+    print "Unknown Error"
+    exit();
+
+def calculateCluster(mode):
+    result = -1
+
+    offset = -1
+    sectorSize = -1
+    logical = -1
+    physical = -1
+
+    clusterSize = -1
+    reservedSectors = -1
+    fatTables = -1
+    fatLength = -1
+
+    offset = mode[0]
+    if(mode[1]):
+        sectorSize = mode[2]
+    else:
+        sectorSize = DEFAULT_SECTOR_SIZE
+    if(mode[3] != -1):
+        logical = mode[3]
+    if(mode[4] != -1):
+        physical = mode[4]
+    if(mode[5] != -1):
+        # this is the only argument we need in this case
+        print mode[5]
+        exit();
+    clusterSize = mode[6]
+    reservedSectors = mode[7]
+    fatTables = mode[8]
+    fatLength = mode[9]
+
+    if(logical == -1 and physical == -1):
+        print 'Error: no address to convert'
+        exit();
+    elif(logical != -1 and cluster != -1):
+        print 'Error conflicting address command, only eneter one known address'
+        exit();
+    elif(clusterSize < 0 or reservedSectors < 0 or fatTables < 0 or fatLength < 0):
+        comma = False;
+        errorMessage = 'Error: '
+        if(clusterSize < 0):
+            if(comma):
+                errorMessage+=', '
+            else:
+                comma = True;
+            errorMessage += 'Invalid cluster size'
+
+            exit()
+        elif(reservedSectors < 0):
+            if(comma):
+                errorMessage+=', '
+            else:
+                comma = True;
+            errorMessage += 'Invalid number of reserved sectors'
+            exit()
+        elif(fatTables < 0):
+            if(comma):
+                errorMessage+=', '
+            else:
+                comma = True;
+            print 'Invalid number of FAT tables'
+            exit();
+        elif(fatLength < 0):
+            if(comma):
+                errorMessage+=', '
+            else:
+                comma = True;
+            print 'Invalid FAT length'
+        print errorMessage
+        exit()
+    elif(logical != -1):
+        result = ((logical - (fatTables * fatLength) - reservedSectors) / clusterSize) + 2
+        print result
+        exit()
+    elif(physical != -1):
+        result = ((physical - (fatTables * fatLength) - reservedSectors - offset) / clusterSize) + 2
+        print result
+        exit()
 # we'll create a list of the modes we find
-def solveModes(self, args):
+def solveModes(args):
     mode = [-1]
 
     mode[0] = findMainMode(mode, args)
-    mode.append(args,'-b', '--partition-start=')
-    # this adds two values to the list
-    mode.extend(byteMode(args))
-    mode.append(args, '-s', '--sector-size=', DEFAULT_SECTOR_SIZE)
-    mode.append(args, '-l', '--logical-known=')
-    mode.append(args, '-p', '--logical-known=')
-    mode.append(args, '-c', '--cluster-known=')
-    mode.append(args, '-k', '--cluster-size=')
-    mode.append(args, '-r', '--reserved=')
-    mode.append(args, '-t', '--fat-tables=', DEFAULT_FAT_TABLES)
-    mode.append(args, '-f', '--fat-length=')
 
-    for element in mode:
-        print element
+    # 0
+    mode.append(parseArgument(args,'-b','--partition-start=', -1))
+    # this adds two values to the list
+    # 1, 2
+    mode.extend(byteMode(args))
+    #mode.append(parseArgument(args, '-s', '--sector-size=', DEFAULT_SECTOR_SIZE))
+    # 3
+    mode.append(parseArgument(args, '-l', '--logical-known=', -1))
+
+    # 4
+    mode.append(parseArgument(args, '-p', '--physical-known=', -1))
+
+    # 5
+    mode.append(parseArgument(args, '-c', '--cluster-known=', -1))
+
+    # 6
+    mode.append(parseArgument(args, '-k', '--cluster-size=', -1 ))
+
+    # 7
+    mode.append(parseArgument(args, '-r', '--reserved=', -1))
+
+    # 8
+    mode.append(parseArgument(args, '-t', '--fat-tables=', DEFAULT_FAT_TABLES))
+
+    # 9
+    mode.append(parseArgument(args, '-f', '--fat-length=', -1))
+
+    return mode
 
 
 def findMainMode(mode, args):
@@ -40,24 +314,12 @@ def findMainMode(mode, args):
     mode = -1
     arg = args[0]
     if arg == '-L' or arg == '--logical':
-        mode = Mode.logical
+        mode = LOGICAL
     elif arg == '-P' or arg == '--physical':
-        mode = Mode.physical
+        mode = PHYSICAL
     elif arg == '-C' or arg == '--cluster':
-        mode = Mode.cluster
+        mode = CLUSTER
     return mode
-
-def getOffset(args):
-    offset = 0
-    pattern = re.compile('--partition-start=')
-    for arg in args:
-        if arg == '-b':
-            index = args.index(arg)
-            offset = int(args[index])
-        elif pattern.match(arg) != None:
-            index = args.index(arg)
-            offset = int(args[index][18:])
-    return offest
 
 def byteMode(args):
     byteMode = [False, DEFAULT_SECTOR_SIZE]
@@ -75,29 +337,18 @@ def byteMode(args):
     return byteMode
 
 
-def parseArgument(args, short, long):
-    value = -1
-    pattern = re.compiler(long)
-    for arg in args:
-        if arg = short:
-            index = args.index(arg)
-            address = int(args[index+1])
-        elif pattern.match(arg) != None:
-            size = int(arg[len(long)])
-
 def parseArgument(args, short, long, default):
     value = default
-    pattern = re.compiler(long)
+    pattern = re.compile(long)
     for arg in args:
-        if arg = short:
+        if (arg == short):
             index = args.index(arg)
-            address = int(args[index+1])
+            value = int(args[index+1])
         elif pattern.match(arg) != None:
-            size = int(arg[len(long)])
+            value = int(arg[len(long):])
+    return value
 
 
-class Mode(Enum):
-    default = 1000
-    logical = 1001
-    physical = 1002
-    cluster = 1003
+
+if __name__== "__main__":
+    main()
